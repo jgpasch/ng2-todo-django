b429e4daa17fdd112968296bae29414aeff73845
John Paschal <jgpasch@gmail.com>
2017-04-19 15:17:57 -0400

    removed cached

--- src/config/server.ts
+++
@@ -1,6 +0,0 @@
-export const serverSettings = {
-  url: 'http://localhost:8000/api',
-  client_id: '3yPN428VSMxaf6Oj7ZJRsixdHX2b0tf5HDGzxuJj',
-  // tslint:disable-next-line:max-line-length
-  client_secret: 'jW2D7KiLXp5o7JoFpQQBA0UkQafnCkHFZDItNAF6P99UgaGEwEGeZJVH7c37FayPGhOznLQaZZoEO9Qdm1AZMjYqrs6RbOHrNEh4LW2XFttnEdi9ITZfJbveFfMjpw6E'
-};
